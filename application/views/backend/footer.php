8b85954c4c1c8643950f8f68d05658e0a7a5edb1
Afrasiab006 <71026295+Afrasiab006@users.noreply.github.com>
2026-07-12 16:57:55 +0500

    First Commit
    First Date: 11-03-2022

---
+++ application/views/backend/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; <?php echo date('Y')?> | EduManage: Complete School Management System with Admin, Teacher, Student & Parent Portals |
+    <strong>Developed by Afrasiab Ahmad</strong>
+</footer>
---
+++ assets/js/neon-skins.js
@@ -0,0 +1,33 @@
+/**
+ *	Neon Demo Scripts (Demo Version Only)
+ *
+ *	Developed by https://github.com/Afrasiab006
+ */
+
+
+;(function($, window, undefined)
+{
+	"use strict";
+	
+	$(document).ready(function()
+	{
+		
+		var domain = $("body").data('url').replace('http://', '').replace('/neon', '').replace('themes.', '');
+		
+		
+		//Cookies.set('current-skin', 'cafe', {domain: domain});
+		
+		$(".theme-skins").on('click', 'li a', function(ev)
+		{
+			ev.preventDefault();
+			
+			var $this = $(this);
+			
+			Cookies.set('current-skin', $this.data('skin'), {domain: domain, expires: 3600});
+			
+			window.location.href = $this.attr('href');
+		});
+	
+	});
+	
+})(jQuery, window);
